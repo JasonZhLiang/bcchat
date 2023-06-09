b6b13dc5ed4437a0e45e35ca799cbc3a9e8bf6f5
Jason Liang <Jasonzh.liang@gmail.com>
2023-06-08 21:56:51 -0400

    Initial Commit

---
+++ bcchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bcchat
+//
+//  Created by Jason Liang on 2023-06-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
